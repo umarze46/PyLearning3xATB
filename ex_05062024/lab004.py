1b496fdef9df8d359cd25acec912794655659e90
umarze46 <umaeze46@gmail.com>
2024-06-05 08:56:59 +0530

    commit 3

---
+++ ex_05062024/lab004.py
@@ -0,0 +1,5 @@
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+print(First_name, Last_name, sep=':', end='.')
+print(age)
