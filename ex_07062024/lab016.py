78b76bb965e546e2df7aecccaf6f130111a43e8c
umarze46 <umaeze46@gmail.com>
2024-06-12 00:30:42 +0530

    commit 16

---
+++ ex_07062024/lab016.py
@@ -0,0 +1,6 @@
+#Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+num = float(input("Enter the number: "))
+square = num ** 2
+cube = num ** 3
+print("Square of the number is: ", square)
+print("Cube of the number is: ", cube)
