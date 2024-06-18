477bf2e3c0e0c125e39fd5a5bba61f982fd65264
umarze46 <umarze46@gmail.com>
2024-06-18 21:55:04 +0530

    Update lab056.py

--- ex_17062024/lab056.py
+++ ex_17062024/lab056.py
@@ -1,9 +1,9 @@
-def interest_default(gf="sana"):
-    print("umar", gf)
+def interest_default(f="sana"):
+    print("umar", f)
     print('love')
 
 
 interest_default()
 interest_default('preet')
-interest_default(gf='kath')
+interest_default(f='kath')
 interest_default('stan')
